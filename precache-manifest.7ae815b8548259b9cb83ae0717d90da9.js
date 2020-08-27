f81cde855fd687e17d1061126146a0b694e20ef9
Elmer Kim <pooshlmer@gmail.com>
2020-08-26 23:47:15 -0700

    Updates

---
+++ precache-manifest.7ae815b8548259b9cb83ae0717d90da9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b6bafd6411c20e82bd2872de38159f",
+    "url": "/twitchclips/index.html"
+  },
+  {
+    "revision": "ed8a978aff20ea2a6787",
+    "url": "/twitchclips/static/css/main.6c061686.chunk.css"
+  },
+  {
+    "revision": "cf77b533d0a45a9106db",
+    "url": "/twitchclips/static/js/2.f64f49de.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/twitchclips/static/js/2.f64f49de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8a978aff20ea2a6787",
+    "url": "/twitchclips/static/js/main.ca2bc9ec.chunk.js"
+  },
+  {
+    "revision": "37d0d54a453622358402",
+    "url": "/twitchclips/static/js/runtime-main.82a9469d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pooshlmer/twitchclips/precache-manifest.8defcb5d6290ec0c6af549d93cb96f10.js"
+  "/twitchclips/precache-manifest.7ae815b8548259b9cb83ae0717d90da9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pooshlmer/twitchclips/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/twitchclips/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
